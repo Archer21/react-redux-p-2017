c5348357272d5ebd64a1fe21d209a4341e78516e
Archer <yatogami18@gmail.com>
2017-01-20 19:02:52 -0400

    first commit

---
+++ source/pages/containers/Post.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+
+class Post extends Component {
+  render () {
+    return (
+      <section name="Post">
+        <h1>Post</h1>
+        <Link to="/">
+          Go to Home
+        </Link>
+
+        <Link to="/random">
+          Go to Random
+        </Link>
+      </section>
+    )
+  }
+}
+
+export default Post
---
+++ source/pages/containers/Profile.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+
+class Profile extends Component {
+  render () {
+    return (
+      <section name="Profile">
+        <h1>Profile</h1>
+        <Link to="/">
+          Go to Home
+        </Link>
+
+        <Link to="/random">
+          Go to Random
+        </Link>
+      </section>
+    )
+  }
+}
+
+export default Profile
